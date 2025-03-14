e836567061fbf8450f5f7c973a0468ba56f43cbe
hrsdemo <hrsdemo2@gmail.com>
2025-03-14 07:35:41 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -51,6 +51,7 @@ const app = express();
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
+app.use(helmet());
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
